85caa45cdd597718a26db84915782fb26d798f30
Alfisyahrin Abd Halek <okkotsusan@outlook.com>
2024-01-18 17:22:28 +0900

    First Push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// Membuat fungsi hitungLuas untuk menghitung luas persegi
 function hitungLuas() {
-    // Mendapatkan nilai sisi persegi dari form
     var sisi = document.getElementById("sisi").value;
-    // Menghitung luas persegi dengan rumus sisi x sisi
+    sisi = parseInt(sisi);
     var luas = sisi * sisi;
-    // Menampilkan hasil perhitungan ke elemen dengan id "hasil"
-    document.getElementById("hasil").innerHTML = "Luas persegi adalah " + luas + " cm<sup>2</sup>";
-}
+    document.getElementById("hasil").innerHTML = "Luas persegi adalah " + luas;
+    console.log("Luas persegi adalah " + luas);
+  }
